88a7217ff306382fb1026e392677a6d2d7c0cf27
mysoftwarechar <118206330+cihangirincaz@users.noreply.github.com>
2024-08-11 02:46:43 +0300

    HomeTableViewCell updated

--- BauBuddy/Module/Home/View/HomeTableViewCell.swift
+++ BauBuddy/Module/Home/View/HomeTableViewCell.swift
@@ -9,7 +9,7 @@ import UIKit
 import SnapKit
 
 class HomeTableViewCell: UITableViewCell {
-
+    let taskLabel = UILabel()
     let titleLabel = UILabel()
     let descriptionLabel = UILabel()
     let colorView = UIView()
@@ -27,36 +27,45 @@ class HomeTableViewCell: UITableViewCell {
     private func setupViews() {
         contentView.addSubview(colorView)
         colorView.snp.makeConstraints { make in
-            make.leading.top.bottom.equalToSuperview().inset(8)
+            make.left.top.bottom.equalToSuperview().inset(8)
             make.width.equalTo(10)
         }
-        contentView.addSubview(titleLabel)
-        titleLabel.snp.makeConstraints { make in
-            make.leading.equalTo(colorView.snp.trailing).offset(8)
-            make.trailing.equalToSuperview().inset(8)
+
+        contentView.addSubview(taskLabel)
+        taskLabel.snp.makeConstraints { make in
+            make.left.equalTo(colorView.snp.right).offset(8)
+            make.right.equalToSuperview().inset(8)
             make.top.equalToSuperview().offset(8)
         }
+
+        contentView.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { make in
+            make.left.equalTo(colorView.snp.right).offset(8)
+            make.right.equalToSuperview().inset(8)
+            make.top.equalTo(taskLabel.snp.bottom).offset(4)
+        }
+
         contentView.addSubview(descriptionLabel)
         descriptionLabel.snp.makeConstraints { make in
-            make.leading.equalTo(titleLabel)
-            make.trailing.equalTo(titleLabel)
+            make.left.equalTo(titleLabel)
+            make.right.equalTo(titleLabel)
             make.top.equalTo(titleLabel.snp.bottom).offset(4)
             make.bottom.equalToSuperview().inset(8)
         }
 
-        
-
-        
-
-        
+        taskLabel.font = UIFont.systemFont(ofSize: 14)
+        taskLabel.numberOfLines = 0
 
         titleLabel.font = UIFont.boldSystemFont(ofSize: 16)
+        titleLabel.numberOfLines = 0
+
         descriptionLabel.font = UIFont.systemFont(ofSize: 14)
         descriptionLabel.numberOfLines = 0
         descriptionLabel.textColor = .gray
     }
 
     func configure(with task: Task) {
+        taskLabel.text = task.task
         titleLabel.text = task.title
         descriptionLabel.text = task.description
         colorView.backgroundColor = UIColor(hex: task.colorCode)
